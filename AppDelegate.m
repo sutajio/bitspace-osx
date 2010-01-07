bd11f0b365a48b1cb5d88f2493eb4a01f7f6ded1
Niklas Holmgren <niklas.holmgren@mindset.se>
2010-01-07 18:47:08 +0100

    Added custom user-agent string.

--- AppDelegate.m
+++ AppDelegate.m
@@ -14,6 +14,13 @@
 @implementation AppDelegate
 
 -(void) awakeFromNib {
+	// Set the user-agent string
+	NSString *userAgent = [NSString stringWithFormat:@"%@ %@",
+						    [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleName"],
+							[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleVersion"]];
+	[webView setApplicationNameForUserAgent: userAgent];
+	
+	// Load the player
 	WebFrame *mainFrame = [webView mainFrame];
 	NSURL *url = [NSURL URLWithString: @"http://bitspace.at/player/"];
 	NSURLRequest *request = [NSURLRequest requestWithURL: url];
